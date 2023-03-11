8e743eee38e476b102f2521c482d1059bf1bc8a0
Ary <ariadna.des.garcia@gmail.com>
2023-03-11 18:28:35 -0300

    polimorfismo

---
+++ src/ControlBonificacion.java
@@ -0,0 +1,12 @@
+
+public class ControlBonificacion {
+	private double suma;
+	
+	//metodos
+	public double registrarSalario(Funcionario funcionario) {
+		this.suma=funcionario.getBonificacion()+this.suma;
+		System.out.println("calculo actual "+this.suma);
+		return suma;
+	}
+	
+}
